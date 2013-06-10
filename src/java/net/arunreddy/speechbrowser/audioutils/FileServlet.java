a845efb2dd23cc08795e23b098c8936852abf63e
Arun Reddy <arunreddy.nelakurthi@gmail.com>
2013-06-10 13:24:00 -0700

    Updated audio streaming servlet

--- src/java/net/arunreddy/speechbrowser/audioutils/FileServlet.java
+++ src/java/net/arunreddy/speechbrowser/audioutils/FileServlet.java
@@ -3,13 +3,59 @@
  */
 package net.arunreddy.speechbrowser.audioutils;
 
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author venus
- *
+ * 
  */
 public class FileServlet extends HttpServlet {
+	static final long serialVersionUID = 1L;
+	private static final int BUFSIZE = 4096;
 
-	
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String filePath = request.getParameter("path");
+		File file = new File(filePath);
+		int length = 0;
+		ServletOutputStream outStream = response.getOutputStream();
+		ServletContext context = getServletConfig().getServletContext();
+		String mimetype = context.getMimeType(filePath);
+
+		System.out.println(mimetype);
+		// sets response content type
+		if (mimetype == null) {
+			mimetype = "application/octet-stream";
+		}
+		response.setContentType(mimetype);
+		response.setContentLength((int) file.length());
+		String fileName = (new File(filePath)).getName();
+
+		// sets HTTP header
+		response.setHeader("Content-Disposition", "attachment; filename=\""
+				+ fileName + "\"");
+
+		byte[] byteBuffer = new byte[BUFSIZE];
+		DataInputStream in = new DataInputStream(new FileInputStream(file));
+
+		// reads the file's bytes and writes them to the response stream
+		while ((in != null) && ((length = in.read(byteBuffer)) != -1)) {
+			outStream.write(byteBuffer, 0, length);
+		}
+
+		in.close();
+		outStream.close();
+	}
+
 }
