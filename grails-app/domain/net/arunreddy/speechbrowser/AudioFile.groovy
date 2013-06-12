85542a6ec8501595dc5e3072b3cf1769a4a896b2
Arun Reddy <arunreddy.nelakurthi@gmail.com>
2013-06-12 16:01:57 -0700

    Merged Audiofileprops with AudioFile

--- grails-app/domain/net/arunreddy/speechbrowser/AudioFile.groovy
+++ grails-app/domain/net/arunreddy/speechbrowser/AudioFile.groovy
@@ -4,20 +4,24 @@ import java.util.Date;
 
 class AudioFile {
 
-	String path
-	String name
-	String mimetype
-	Date dateCreated
-	Date lastUpdated
-	
-	static belongsTo = [corpusDir: CorpusDir]
-			
+    String path
+    String name
+    String mimetype
+    int channels
+    int sampleRate
+    int bitDepth
+    long frames
+    int duration
+    Date dateCreated
+    Date lastUpdated
+
+    static belongsTo = [corpusDir: CorpusDir]
+
     static constraints = {
-		path(blank: false)
-		name(blank: false)
-		mimetype(blank:false)
+        path(blank: false)
+        name(blank: false)
+        mimetype(blank:false)
     }
-	
-	static mapping = { autoTimestamp true }
-	
+
+    static mapping = { autoTimestamp true }
 }
